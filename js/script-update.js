581911344e63670cfe9c7fc6cdf59dac58fd2a67
Sumit Gurung <sumit@Sumits-MacBook-Pro.local>
2024-09-24 17:22:16 +0100

    vist test

--- js/script-update.js
+++ js/script-update.js
@@ -15,3 +15,20 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+
+
+// visit counter
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
